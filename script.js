808fcb54ef7f642fbbc7995aa535ed937cd64013
Prathamesh Patil <patilprathameshbal@gmail.com>
2021-11-03 09:31:24 +0530

    website

---
+++ script.js
@@ -0,0 +1,8 @@
+//javascript//
+var navLinks =document.getElementaryById("navLinks");
+	function showMenu(){
+		navLinks.style.right="0";
+	}
+	function hideMenu(){
+		navLinks.style.right="-200px";
+	}
